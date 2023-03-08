19470022b06c9ff7a6e1dfb0882332d068d1b368
jwbla <jwbla@users.noreply.github.com>
2023-03-08 13:12:17 -0500

    update to do first http call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private http: HttpClient
+  ) {}
+  ngOnInit() {
+    this.http.get('https://pokeapi.co/api/v2/pokemon/ditto').subscribe( data => console.log('data: ', data));
+  }
   title = 'obs-poke';
 }
